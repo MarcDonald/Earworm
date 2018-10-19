0de4d4a7e4103ab731a3aa3a500dd28b34f5a545
Marc <marcsdeveloperemail@gmail.com>
2018-10-19 13:36:24 +0100

    Add RecyclerView Adapter and ViewHolder for Main Screen
    -Currently using an array of Strings to test that it works

--- app/src/main/java/app/marcdev/earworm/mainscreen/MainFragment.kt
+++ app/src/main/java/app/marcdev/earworm/mainscreen/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import app.marcdev.earworm.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -18,6 +19,7 @@ class MainFragment : Fragment()
     Timber.d("Log: onCreateView: Started")
     val view = inflater.inflate(R.layout.fragment_mainscreen, container, false)
     bindViews(view)
+    setupRecycler(view)
 
     return view
   }
@@ -27,12 +29,18 @@ class MainFragment : Fragment()
     Timber.v("Log: bindViews: Started")
     val fab: FloatingActionButton = view.findViewById(R.id.fab_main)
     fab.setOnClickListener(fabOnClickListener)
-
-    val recycler: RecyclerView = view.findViewById(R.id.recycler_main)
   }
 
   private val fabOnClickListener = View.OnClickListener {
     Timber.d("Log: fabOnClickListener: Started")
     Toast.makeText(activity, "Fab Clicked", Toast.LENGTH_SHORT).show()
+  }
+
+  private fun setupRecycler(view: View)
+  {
+    Timber.v("Log: setupRecycler: Started")
+    val recycler: RecyclerView = view.findViewById(R.id.recycler_main)
+    recycler.adapter = MainRecyclerAdapter(context)
+    recycler.layoutManager = LinearLayoutManager(context)
   }
 }
